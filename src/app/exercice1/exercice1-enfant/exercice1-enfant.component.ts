cf2643c92c47c41d8879c276ebb3d6e22bf0c4c8
Charlotte Marion <marion.charlotte@orange.fr>
2022-04-17 10:40:04 +0200

    exercice 1

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-exercice1-enfant',
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercice1-enfant.component.css']
 })
 export class Exercice1EnfantComponent implements OnInit {
+  @Input() public result: number = 0;
+  @Output("changementResult") public changement = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
   }
+
+public Add() {
+  this.result++;
+  this.changement.emit({
+    value: this.result,
+  });
+}
+
+public Remove() {
+  this.result--;
+  this.changement.emit({
+    value: this.result,
+  });
+}
 
 }
--- src/app/exercice1/exercice1.component.ts
+++ src/app/exercice1/exercice1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-exercice1',
@@ -6,11 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercice1.component.css']
 })
 export class Exercice1Component implements OnInit {
-  public compteur: number = 0; 
+  public compteur: number = 0;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  MiseAJourCompteur(event: { value: number }) {
+    this.compteur = event.value;
+  }
 }
